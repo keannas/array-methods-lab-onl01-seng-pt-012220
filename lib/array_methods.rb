07c74b4e65232e640906775135e6d0bf637ca0f6
Keanna Salter <keannasalter91@gmail.com>
2020-01-30 06:50:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,21 +3,21 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-  array.include.sort
+  
 end
 
 def using_reverse(array)
-  array.include.reverse
+  
 end
 
 def using_first(array)
-  array.include.first
+  
 end
 
 def using_last(array)
-  array.include.last
+  
 end
 
 def using_size(array)
-  array.include.size
+  
 end
